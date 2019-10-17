aaa8688594be12baafbf0f0fd4943a8a70961669
Rahzel <fkovacs088@gmail.com>
2019-10-17 17:37:45 +0200

    just

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/model/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('any', 'test desc','https://www.nosalty.hu/files/imagecache/recept/recept_kepek/img_2400.jpg')
+  ];
+  
   constructor() { }
 
   ngOnInit() {
